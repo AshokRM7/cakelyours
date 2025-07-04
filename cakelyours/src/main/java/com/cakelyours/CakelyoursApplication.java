eaf22fd6a330c2e135e1d765ad11098a945d9a83
AshokRM7 <ashoksplspl7@gmail.com>
2025-07-04 18:34:49 +0530

    Initial commit for cakelyours website

---
+++ cakelyours/src/main/java/com/cakelyours/CakelyoursApplication.java
@@ -0,0 +1,13 @@
+package com.cakelyours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakelyoursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakelyoursApplication.class, args);
+	}
+
+}
